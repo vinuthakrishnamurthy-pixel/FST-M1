667d765096083c360ddddafb39131eec5f6a9c3c
vinuthakrishnamurthy-pixel <vinuthakrishnamurthy@gmail.com>
2026-06-15 09:20:55 +0530

    Add files via upload

--- Java/Activities/Activity3.java
+++ Java/Activities/Activity3.java
@@ -1,10 +1,24 @@
 package activites;
 
+enum Device {
+	LIGHT,
+	THERMOSTAT
+}
+
 public class Activity3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static String adjustDevice(Device device, int value) {
+		String status = switch (device) {
+		case null -> "Invalid device type";
+		case Device d when d.equals(Device.THERMOSTAT) && value > 40 -> "[THERMOSTAT] Warning: Temperature too high!";
+		case THERMOSTAT -> "[THERMOSTAT] Temp is set to " + value;
+		case LIGHT -> "[LIGHT] Adjusting brightness to " + value + "%";
+		default -> "Unknown device";
+		};
+		
+		return status;
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println(adjustDevice(Device.THERMOSTAT, 50));
+	}
 }
